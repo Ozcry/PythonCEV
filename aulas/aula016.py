98ff6b9d7fac19b8a5107cf277741a55f2d98d32
Jean Lucas <jean.lucasferreira1998@gmail.com>
2021-04-02 10:51:28 -0300

    att

---
+++ aulas/aula016.py
@@ -0,0 +1,37 @@
+### Variaveis compostas TUPLAS
+'''print(lanche[2])'''
+'''print(lanche[0:2])'''
+'''print(lanche[1:])'''
+'''print(lanche[-1])'''
+'''len(lanche)'''
+'''for c in lanche:
+    print(c)'''
+
+###
+''''As tuplas são IMUTÁVEIS'''
+
+### Brincando
+lanche = ('Hambúrger', 'Suco', 'Pizza', 'Pudím')
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b +a
+pessoa = ('Jean', 23, 'M', 65.5)
+
+'''print(lanche[1])'''
+
+'''for c in lanche:
+    print(f'Eu vou comer {c}')'''
+
+'''for c in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[c]} na posição {c}')'''
+
+'''for pos, c in enumerate(lanche):
+    print(f'Eu vou comer {c} na posição {pos}')'''
+
+'''print(sorted(lanche))'''
+
+'''print(c.count(5))'''
+
+'''print(c.index(5, 1))'''
+
+'''del pessoa'''
