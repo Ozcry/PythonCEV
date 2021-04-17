ceca7954f9063f4c19b77be55cab5e1d081733b8
Jean Lucas <jean.lucasferreira1998@gmail.com>
2021-04-17 12:27:42 -0300

    att

---
+++ aulas/aula023.py
@@ -0,0 +1,37 @@
+### Tratamentos de erros e Exceções
+'''
+print(x) -> (exceção) erro semantico
+primt(x) -> erro sintatico
+'''
+
+'''
+try:
+    operação
+except:
+    falou
+else:
+    deu certo
+finally:
+    certo/falha
+'''
+
+# Bricando
+
+'''
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou.')
+except ZeroDivisionError:
+    print('Não e possivel dividir um número por zero!')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados!')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__cause__}')
+else:
+    print(f'O resultado é {r:.1f}')
+finally:
+    print('Volte sempre! Muito Obrigado!')
+'''
---
+++ ex/ex113.py
@@ -0,0 +1,2 @@
+'''Reescreva a função leiaInt() que fizemos no DESAFIO 104, incluindo agora a possibilidade da digitação de um número de
+tipo inválido. Aproveite e crie também uma função leiaFloot() com a mesma funcionalidade.'''
